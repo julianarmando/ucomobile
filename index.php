81ee29f48a13a96c5e2d2ccd4bddfb1929d6f792
Julian Bohorquez <julianarmando@gmail.com>
2013-12-06 12:33:59 -0500

    Primer comit con el archivo html funcionando, version basica y preliminar de la aplicación

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html> 
+<html> 
+<head> 
+	<title>Ubuntu Colombia</title> 
+	<meta name="viewport" content="width=device-width, initial-scale=1"> 
+	<link rel="stylesheet" href="http://code.jquery.com/mobile/1.2.1/jquery.mobile-1.2.1.min.css" />
+	<script src="http://code.jquery.com/jquery-1.8.3.min.js"></script>
+	<script src="http://code.jquery.com/mobile/1.2.1/jquery.mobile-1.2.1.min.js"></script>
+</head> 
+<body> 
+
+<div data-role="page">
+
+	<div data-role="header">
+		<h1>Ultimos Tweets en @ubuntuco</h1>
+	</div><!-- /header -->
+
+	<div data-role="content">	
+		<p style="text-align: center;">
+
+				<a class="twitter-timeline" href="https://twitter.com/ubuntuco" data-widget-id="258366830840774656" lang="ES" data-chrome="noheader nofooter noscrollbar"></a>
+				<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");
+				</script>
+
+		</p>		
+	</div><!-- /content -->
+
+</div><!-- /page -->
+
+</body>
+</html>
